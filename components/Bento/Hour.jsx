818b71ccc7ba33527f2c6a889f5075bea7b3fcbd
LilNait-S <style.damn.falling@gmail.com>
2023-08-04 01:11:37 -0500

    added dinamic date

--- components/Bento/Hour.jsx
+++ components/Bento/Hour.jsx
@@ -1,13 +1,31 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
 const Hour = () => {
-  const formatedHours = new Date(Date.now()).toLocaleString("es-ES", {
-    hour: "2-digit",
-    minute: "2-digit",
-    hour12: true,
-  });
+  const [formattedHour, setFormattedHour] = useState("");
+
+  useEffect(() => {
+    // Obtenemos la hora local del cliente
+    const clientDate = new Date();
+
+    // Obtenemos la zona horaria del cliente
+    const clientTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+    // Formateamos la hora usando la zona horaria del cliente
+    const formatedHours = clientDate.toLocaleString("es-ES", {
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true,
+      timeZone: clientTimeZone,
+    });
+
+    setFormattedHour(formatedHours);
+  }, []);
 
   return (
     <span className="text-3xl flex font-bold uppercase justify-center items-center h-full">
-      {formatedHours}
+      {formattedHour}
     </span>
   );
 };
--- components/Bento/LargeDate.jsx
+++ components/Bento/LargeDate.jsx
@@ -1,19 +1,40 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
 const LargeDate = () => {
-  const longDate = new Date(Date.now()).toLocaleString("es-ES", {
-    day: "2-digit",
-    month: "long",
-    year: "numeric",
-  });
-  const weekday = new Date(Date.now()).toLocaleString("es-ES", {
-    weekday: "long",
-  });
+  const [formattedDate, setFormattedDate] = useState("");
+  const [weekday, setWeekday] = useState("");
+
+  useEffect(() => {
+    // Obtenemos la fecha y el día de la semana local del cliente
+    const clientDate = new Date();
+    const clientTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+    // Formateamos la fecha usando la zona horaria del cliente
+    const longDate = clientDate.toLocaleString("es-ES", {
+      day: "2-digit",
+      month: "long",
+      year: "numeric",
+      timeZone: clientTimeZone,
+    });
+
+    const clientWeekday = clientDate.toLocaleString("es-ES", {
+      weekday: "long",
+      timeZone: clientTimeZone,
+    });
+
+    setFormattedDate(longDate);
+    setWeekday(clientWeekday);
+  }, []);
+
   return (
     <div className="flex justify-center items-center h-full">
       <div className="flex flex-col items-start justify-center">
         <span className="font-bold purple_gradient capitalize text-xl leading-5">
           {weekday}
         </span>
-        <span>{longDate}</span>
+        <span>{formattedDate}</span>
       </div>
     </div>
   );
